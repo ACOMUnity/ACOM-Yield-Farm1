b1acbb8e8eb7718d27d9e6bdf12dade49989b7f1
ACOMUnity <77648984+ACOMUnity@users.noreply.github.com>
2021-01-22 15:01:56 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
     ropsten: { //Tested, seems to work fine
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
         network_id: 3,       // Ropsten's id
-        gas: 662260,        // Ropsten has a lower block limit than mainnet
+        gas: 70000,        // Ropsten has a lower block limit than mainnet
         confirmations: 0,    // # of confs to wait between deployments. (default: 0)
         timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
         skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
